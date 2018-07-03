aa8b29d4e146529443f38780586beec95ccb2feb
akash <akash4492@yahoo.com>
2018-07-03 21:52:50 +0530

    Initial design of VirtualMachine
    VirtualMachine class is implemented according to intial design.

--- src/com/akash/dobaria/VirtualMachine.java
+++ src/com/akash/dobaria/VirtualMachine.java
@@ -1,5 +1,81 @@
 package com.akash.dobaria;
 
-public class VirtualMachine {
+import java.util.HashMap;
 
+public class VirtualMachine {
+    private MemoryBlock[] virtualMemory;
+    private HashMap<String, Byte> symbolTable;
+    private int registerA;
+    private int registerB;
+    private byte programCounter;
+    private boolean zeroResultBit;
+    private boolean overflowBit;
+
+    public VirtualMachine(){
+        virtualMemory = new MemoryBlock[256];
+        symbolTable = new HashMap<String, Byte>();
+        registerA = 0;
+        registerB = 0;
+        programCounter = 0;
+        zeroResultBit = false;
+        overflowBit = false;
+    }
+
+    public int getRegisterA() {
+        return registerA;
+    }
+
+    public int getRegisterB() {
+        return registerB;
+    }
+
+    public byte getProgramCounter() {
+        return programCounter;
+    }
+
+    public boolean getZeroResultBit() {
+        return zeroResultBit;
+    }
+
+    public boolean getOverflowBit() {
+        return overflowBit;
+    }
+
+    public int getIndex(String symbol){
+        if(!symbolTable.containsKey(symbol)) return -1;
+        return symbolTable.get(symbol);
+    }
+
+    public MemoryBlock getMemoryatIndex(int index){
+        if(index<0 || index>virtualMemory.length) return null;
+        return virtualMemory[index];
+    }
+
+    public void setRegisterA(int registerA) {
+        this.registerA = registerA;
+    }
+
+    public void setRegisterB(int registerB) {
+        this.registerB = registerB;
+    }
+
+    public void setProgramCounter(byte programCounter) {
+        this.programCounter = programCounter;
+    }
+
+    public void setZeroResultBit(boolean zeroResultBit) {
+        this.zeroResultBit = zeroResultBit;
+    }
+
+    public void setOverflowBit(boolean overflowBit) {
+        this.overflowBit = overflowBit;
+    }
+
+    public void addSymbol(String symbol, byte index) {
+        this.symbolTable.put(symbol,index);
+    }
+
+    public void storeInMemory(MemoryBlock ablock, int index){
+        this.virtualMemory[index] = ablock;
+    }
 }
