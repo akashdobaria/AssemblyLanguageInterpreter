e057d68cd33aa69689e3ae0f10bd05c2e52d8ac5
akash <akash4492@yahoo.com>
2018-07-08 19:21:22 +0530

    Implementation of MemoryBlock class
    Implemented MemoryBlock class: constructors and methods.

--- src/com/akash/dobaria/MemoryBlock.java
+++ src/com/akash/dobaria/MemoryBlock.java
@@ -1,4 +1,53 @@
 package com.akash.dobaria;
 
+enum TYPE{
+    NULL, INSTRUCTION, VALUE;
+}
+
 public class MemoryBlock {
+    private int data;
+    private String instruction;
+    private TYPE dataType;
+
+    public MemoryBlock(){
+        data = 0;
+        instruction = null;
+        dataType = TYPE.NULL;
+    }
+
+    public MemoryBlock(int aNumber){
+        data = aNumber;
+        instruction = null;
+        dataType = TYPE.VALUE;
+    }
+
+    public MemoryBlock(String anInstruction){
+        data = -1;
+        instruction = anInstruction;
+        dataType = TYPE.INSTRUCTION;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public TYPE getDataType() {
+        return dataType;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public void setDataType(TYPE dataType) {
+        this.dataType = dataType;
+    }
 }
